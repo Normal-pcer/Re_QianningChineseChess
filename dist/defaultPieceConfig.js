60140e44a801f5cfa285f805530f15fa657c0b5c
normal-pcer <normal-pcer@outlook.com>
2024-08-25 13:54:38 +0800

    忘了编译了，蚌

--- dist/defaultPieceConfig.js
+++ dist/defaultPieceConfig.js
@@ -24,5 +24,6 @@ export const defaultPieceConfigs = {
     horse: new PieceConfig(3000, 2750, 200, DamageType.MeleeMedium),
     chariot: new PieceConfig(4500, 2200, 200, DamageType.MeleeHigh),
     gun: new PieceConfig(5000, 2250, 200, DamageType.Ranged),
+    pawn: new PieceConfig(2250, 1250, 100, DamageType.MeleeLow),
 };
 //# sourceMappingURL=defaultPieceConfig.js.map
